2366f0663dfbf3fdfe7e2ed37f1e931fb2e81cfd
karen cofre <karencofrecejas@gmail.com>
2022-07-14 08:45:25 -0400

    Initial Commit

---
+++ NotasWatchApp WatchKit Extension/NotasWatchAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotasWatchAppApp.swift
+//  NotasWatchApp WatchKit Extension
+//
+//  Created by karen cofré on 14-07-22.
+//
+
+import SwiftUI
+
+@main
+struct NotasWatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
